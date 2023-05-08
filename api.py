d0b4eb5a4c8c7a988fde941c84576ec02e1e2051
big-vi <vignesh.murugan@hotmail.com>
2023-05-08 17:03:47 +0530

    connect db

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+import config
+import pyodbc
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +8,15 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
+    connection = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        f'SERVER={config.db_url};'
+        f'DATABASE={config.db_name};'
+        f'UID={config.db_user};'
+        f'PWD={config.db_pass}',
+        autocommit=True
+    )
+    print(connection)
     return "Server is running..."
 
 if __name__ == '__main__':
